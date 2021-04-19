d45babfc105d10881176f2bb610bcb6c9111ea6a
Karan <pate1228@sheridancollege.ca>
2021-04-19 02:11:58 -0400

    Restarting the codebase because of git issues

--- App.js
+++ App.js
@@ -4,10 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Final test commit hopefully to sync</Text>
-      <StatusBar style="auto" />
-    </View>
+
+      <View style={styles.container}>
+          <Text>Final test commit hopefully to sync</Text>
+          <StatusBar style="auto" />
+          <View style={styles.rectangle}></View>
+      </View>
+    
   );
 }
 //test 2
@@ -16,8 +19,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#6C78B8',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  rectangle:{
+      position: "absolute",
+      width: 411,
+      height: 269,
+      left: 0,
+      top: 0,
+      backgroundColor: "#FFFFFF",
+      borderTopLeftRadius: 0,
+      borderTopRightRadius: 0,
+      borderBottomRightRadius: 55,
+      borderBottomLeftRadius: 55
   },
 });
