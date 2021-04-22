ae99997b223788a98b2971d5c609f83765791973
Karan <pate1228@sheridancollege.ca>
2021-04-22 15:36:06 -0400

    AuthScreen and changes to item navigator

--- navigation/ItemsNavigator.js
+++ navigation/ItemsNavigator.js
@@ -1,12 +1,14 @@
 import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-
+import { createSwitchNavigator} from 'react-navigation';
 import ItemsListScreen from '../screens/ItemsListScreen';
 import ItemsDetailScreen from '../screens/ItemDetailScreen';
 import NewItemScreen from '../screens/NewItemScreen';
 import MapScreen from '../screens/MapScreen';
+import AuthScreen from "../screens/AuthScreen"
 import Colors from '../Colours';
+import { forModalPresentationIOS } from 'react-navigation-stack/lib/typescript/src/vendor/TransitionConfigs/CardStyleInterpolators';
 
 const ItemsNavigator = createStackNavigator({
     Items: ItemsListScreen,
@@ -22,7 +24,15 @@ const ItemsNavigator = createStackNavigator({
     }
 });
 
-export default createAppContainer(ItemsNavigator);
+const AuthNavigator = createStackNavigator({
+Auth:AuthScreen
+});
+
+const MainNavigator = createSwitchNavigator({
+    Auth: AuthNavigator,
+    Item: ItemsNavigator
+});
+export default createAppContainer(MainNavigator);
 
 
 
