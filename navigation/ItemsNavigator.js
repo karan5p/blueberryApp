174d1d6751997f9e4d4c404eb2c08e2f55f72168
Deep <deekay1410@gmail.com>
2021-04-22 16:31:05 -0400

    Deleted some files to redo functionality

--- navigation/ItemsNavigator.js
+++ navigation/ItemsNavigator.js
@@ -1,14 +1,12 @@
 import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createSwitchNavigator} from 'react-navigation';
+
 import ItemsListScreen from '../screens/ItemsListScreen';
 import ItemsDetailScreen from '../screens/ItemDetailScreen';
 import NewItemScreen from '../screens/NewItemScreen';
 import MapScreen from '../screens/MapScreen';
-import AuthScreen from "../screens/AuthScreen"
 import Colors from '../Colours';
-import { forModalPresentationIOS } from 'react-navigation-stack/lib/typescript/src/vendor/TransitionConfigs/CardStyleInterpolators';
 
 const ItemsNavigator = createStackNavigator({
     Items: ItemsListScreen,
@@ -24,16 +22,4 @@ const ItemsNavigator = createStackNavigator({
     }
 });
 
-const AuthNavigator = createStackNavigator({
-Auth:AuthScreen
-});
-
-const MainNavigator = createSwitchNavigator({
-    Auth: AuthNavigator,
-    Item: ItemsNavigator
-});
-export default createAppContainer(MainNavigator);
-
-
-
-
+export default createAppContainer(ItemsNavigator);
