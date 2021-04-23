191d60e43abb6e190bb9001fdbce79d80d50df81
Karan <pate1228@sheridancollege.ca>
2021-04-23 00:44:06 -0400

    Finished Sign up page + firebased

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
-import {TouchableOpacity} from 'react-native';
-import {Text, Button, Input} from 'react-native-elements'
+import { StyleSheet } from 'react-native';
+import {View, Text, TouchableOpacity, Platform, ScrollView, Image} from 'react-native';
+import {Button, Input} from 'react-native-elements'
+import {windowHeight, windowWidth} from '../components/Dimentions';
 import firebase from '../firebase/fire';
+import FormInput from '../components/FormInput';
+import FormButton from '../components/FormButton';
 
 const SignupScreen = ({navigation})=>{
     const [email,setEmail]= useState('');
@@ -17,20 +21,93 @@ const SignupScreen = ({navigation})=>{
             setError(err.message);
         }
     }
-    return <>
-        <Input label="Email" value={email} onChangeText={setEmail}></Input>
-        <Input label="Password" value={password} onChangeText={setPassword} secureTextEntry></Input>
-        {
-            error?
-            <Text style={{color:'red'}}>{error}</Text>
-            :null
-        }
-        <Button title="SignUp" onPress={()=> signUp()}></Button>
-        <TouchableOpacity onPress={()=>navigation.navigate('Signin')}>
-            <Text>Already have an account? Sign in</Text>
-        </TouchableOpacity>
+        return <ScrollView contentContainerStyle={styles.container}>
+                <View style={styles.container}>
+                    <View style={styles.purpleCasing}>
+                        <View style={styles.imageView}>
+                            <Image
+                                source={require('../components/blueberry.png')}
+                                style={styles.logo}
+                            />
+                        </View>
+                        <FormInput label="Email" value={email} onChangeText={setEmail}  placeholderText="Email"
+                                iconType="user"
+                                keyboardType="email-address"
+                                autoCapitalize="none"
+                                autoCorrect={false}></FormInput>
+                        <FormInput label="Password" value={password} onChangeText={setPassword} placeholderText="Password"
+                            iconType="lock"
+                            secureTextEntry={true}></FormInput>
+                        <FormButton
+                              buttonTitle="Sign Up"
+                         onPress={() => signUp()}
+                        />
+                     <View style={styles.signIn}>
+                        <TouchableOpacity  onPress={()=>navigation.navigate('Signin')}>
+                        <Text style={styles.navButtonText}>Already have an account? Sign in</Text>
+                        </TouchableOpacity>
+                     </View>
+                    </View>
+                    
+                  
+                    {
+                        error ?
+                            <Text style={{ color: 'red' }}>{error}</Text>
+                            : null
+                    }
+                    
+                </View>
+            </ScrollView>
 
-    </>;
+
 };
+SignupScreen.navigationOptions = {
+    headerTitle: 'Sign Up'
+};
+const styles = StyleSheet.create({
+ 
+   
+    purpleCasing:{
+        width: 411,
+        height: 500,
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0,
+        borderBottomLeftRadius: 50,
+        borderBottomRightRadius: 50,
+        backgroundColor:'#6C78B8'
+    },
+    navButtonText: {
+        fontStyle: 'italic',
+        textDecorationLine: 'underline',
+        fontSize: 18,
+        fontWeight: '500',
+        color: '#fff',
+        marginTop: 20,
+      },
+    signIn:{
+        marginTop: 5,
+        width: '100%',
+        height: windowHeight / 15,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 50,
+    },
+    imageView:{
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo: {
+        height: 200,
+        width: 200,
+        resizeMode: 'cover',
+      },
+      container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF',
+       
+      },
+  });
 
 export default SignupScreen;
