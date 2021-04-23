b24713703f60021e2a5aa7529b2ecafd411496c9
Deep <deekay1410@gmail.com>
2021-04-22 22:40:20 -0400

    Fixed import due to bugs

--- components/database.js
+++ components/database.js
@@ -1,8 +1,8 @@
-import { SQLite } from 'expo-sqlite';
+import * as SQLite from 'expo-sqlite';
 
 const db = SQLite.openDatabase('items.db');
 
-const init = () => {
+export const init = () => {
     const promise = new Promise((resolve, reject) => {
         db.transaction((tx) => {        
             tx.executeSql('CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, imageUri TEXT NOT NULL, address TEXT NOT NULL, latitude REAL NOT NULL, longitude REAL NOT NULL);', 
