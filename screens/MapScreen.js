801947865d91fcb3821c49fccd5c4fc673019399
Deep <deekay1410@gmail.com>
2021-04-23 14:13:22 -0400

    Added markers capability on the map screen, next objective is to be able to save that location

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker }  from 'react-native-maps';
 
 const MapScreen = props => {
+    const [selectedLocation, setSelectedLocation] = useState();
+
     const mapRegion = {
         latitude: 43.642927,
         longitude: -79.387202,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421
     };
-    return <MapView style = {styles.map} region = {mapRegion}/>;
+
+    const selectLocationHandler = event => {
+        setSelectedLocation({
+            latitude: event.nativeEvent.coordinate.latitude,
+            longitude: event.nativeEvent.coordinate.longitude
+        });
+
+        //console.log(event); to check for which data comes up when tapping on the map screen
+    };
+
+    let markerCoordinates;
+
+    if (selectedLocation) {
+        markerCoordinates = {
+            latitude: selectedLocation.latitude,
+            longitude: selectedLocation.longitude
+        };
+    }
+
+
+    return <MapView style = {styles.map} region = {mapRegion} onPress = {selectLocationHandler}>
+        {markerCoordinates && <Marker title = 'Picked Location' coordinate = {markerCoordinates}></Marker>}
+    </MapView>;
 };
 
 const styles = StyleSheet.create({
