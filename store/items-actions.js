3329174463da955836cdac98cab608e97218bd59
Deep <deekay1410@gmail.com>
2021-04-22 20:48:17 -0400

    Set up file systems for data storage, implementing sql database is the next objective

--- store/items-actions.js
+++ store/items-actions.js
@@ -1,5 +1,26 @@
+import * as FileSystem from 'expo-file-system';
+
+
+
+
 export const ADD_ITEM = 'ADD_ITEM';
 
 export const addItem = (title, image) => {
-    return { type: ADD_ITEM, itemData: { title: title, image: image} };
-}
+    return async dispatch => {
+        //converts a long string into parts using / as the target. Array of string segments will be made and retrieve the last segment
+        const fileName = image.split('/').pop();
+        const newPath = FileSystem.documentDirectory + fileName;
+        
+        try{        
+            FileSystem.moveAsync({
+                from: image,
+                to: newPath
+            });
+        } catch (error){
+            console.log(error);
+            throw error;
+        }
+
+        dispatch({ type: ADD_ITEM, itemData: { title: title, image: newPath} });
+    };
+};
